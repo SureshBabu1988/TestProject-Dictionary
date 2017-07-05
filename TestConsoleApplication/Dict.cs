fa2a828ffd38fadb8d1cc013d001e94b0a123ba5
suresh <suresh@VAIO>
2017-07-05 09:12:28 +0530

    Initial Commit

---
+++ TestConsoleApplication/Dict.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;  
+using System.Threading.Tasks;
+
+namespace TestConsoleApplication
+{
+    class CustomDict<K, V> : IEnumerable<KeyValuePair<K, V>>
+    {
+        private readonly int Initial_Size = 16;
+        LinkedList<KeyValuePair<K, V>>[] values = null;
+
+        public CustomDict()
+        {
+            values = new LinkedList<KeyValuePair<K, V>>[Initial_Size];
+        }
+
+        public CustomDict(int size)
+        {
+            Initial_Size = size;
+            values = new LinkedList<KeyValuePair<K, V>>[Initial_Size];
+        }
+
+        public int Count { get; private set; }
+        public int Capacity { get { return values.Length; } }
+
+        public void Add(K key, V value)
+        {
+            var hash = this.HashKey(key);
+
+            if (this.values[hash] == null)
+            { this.values[hash] = new LinkedList<KeyValuePair<K, V>>(); }
+
+            bool KeyAlreadyExists = this.values[hash].Any(p => p.Key.Equals(key));
+
+            if (KeyAlreadyExists)
+            { throw new InvalidOperationException("key with same name already exists. !"); }
+            var pair = new KeyValuePair<K, V>(key, value);
+            this.values[hash].AddLast(pair);
+            this.Count++;
+
+            if (this.Count >= (this.Capacity * 0.8))
+            { this.ReSizeAndReAddValues(); }
+        }
+
+        public V Find(K key)
+        {
+            var hash = this.HashKey(key);
+            if (this.values[hash] == null)
+            { return default(V); }
+
+            return this.values[hash].FirstOrDefault(p => p.Key.Equals(key)).Value;
+        }
+
+        public bool ContainsKey(K key)
+        {
+            var hash = this.HashKey(key);
+            if (this.values[hash] == null)
+            { return false; }
+
+            return this.values[hash].Any(p => p.Key.Equals(key));
+        }
+
+        private int HashKey(K key)
+        {
+            //Math.abs used to get positive numbs..
+            var hashedKey = Math.Abs(key.GetHashCode()) % Capacity;
+            return hashedKey;
+        }
+
+        private void ReSizeAndReAddValues()
+        {
+            //cache the values
+            var cahedValues = this.values;
+
+            // resize
+            this.values = new LinkedList<KeyValuePair<K, V>>[2 * this.Capacity];
+
+            //read the values
+            //this.values = cahedValues;
+
+            foreach (var collection in cahedValues)
+            {
+                if (collection != null)
+                {
+                    foreach (var value in collection)
+                    { this.Add(value.Key, value.Value); }
+                }
+            }
+
+        }
+
+
+        public IEnumerator<KeyValuePair<K, V>> GetEnumerator()
+        {
+            foreach (var collection in this.values)
+            {
+                if (collection != null)
+                {
+                    foreach (var value in collection)
+                    {
+                        yield return value;
+                    }
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ TestConsoleApplication/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Dictionary<int,string> MyDictionary = new Dictionary<int, string>();
+
+            var MyDictionary = new CustomDict<int, string>(25);
+
+            for (int i = 0; i < 5; i++)
+            {
+                MyDictionary.Add(i + 1, "Bunny " + (i + 1));
+            }
+
+            foreach (var bunny in MyDictionary)
+            {
+                Console.WriteLine("K :{0}, V:{1}", bunny.Key, bunny.Value);
+            }
+
+            //Console.WriteLine(MyDictionary.Find(10));
+            Console.WriteLine(MyDictionary.ContainsKey(10));
+            
+            //Action<string> println = Console.WriteLine;
+            //string str = "mystring";
+            //string newString = str.Substring(3);
+            //stringChange(str);
+            //Console.WriteLine(newString);
+            //Console.WriteLine(str);
+
+
+            //StringBuilder strB = new StringBuilder("mystring");
+            //StringBuilder newString1 = strB.("Suresh");
+            //stringBuilderChange(ref strB);
+
+            //if (strB == newString1)
+            //{ println("String builders are equal"); }
+            //Console.WriteLine(newString1);
+            //Console.WriteLine(strB);
+
+
+            Console.ReadLine();
+        }
+
+        public static void stringChange(string teststring)
+        {
+            teststring = "changed";
+        }
+
+        public static void stringBuilderChange(ref StringBuilder teststring)
+        {
+           // teststring = new StringBuilder("Hello");
+            teststring.Append("changed");
+        }
+
+
+    }
+}
